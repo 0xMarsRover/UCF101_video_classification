7e7ad03f2daf40410aa75b98afc65d0ceb926405
Kaiqiang <kaiqianghuang666@gmail.com>
2018-10-29 16:59:56 +0000

    change launch_validate_cnn.sh

--- launch_validate_cnn.sh
+++ launch_validate_cnn.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-#sbatch --job-name=KQH_extract --gres=gpu:1 --mem=32768 --cpus-per-task=4 launch_validate_cnn.sh
-#SBATCH --job-name=KQH_train
-#SBATCH --gres=gpu:0
-#SBATCH --mem=32768
-#SBATCH --cpus-per-task=4
+#sbatch --job-name=KQH_val_cnn --gres=gpu:1 --mem=32768 --cpus-per-task=4 launch_validate_cnn.sh
 
 python3 validate_cnn.py
