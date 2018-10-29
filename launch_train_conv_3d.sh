6d2abff8ec6c330c9fcb5198aa5bc0dcd36e3033
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-10-29 21:19:18 +0000

    Add mlp, c3d, conv_3d .sh

---
+++ launch_train_conv_3d.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#sbatch --job-name=KQH_conv3d --gres=gpu:1 --mem=65536 --cpus-per-task=4 --output=./output/output_train_conv_3d.out launch_train_conv_3d.sh
+
+
+python3 train.py
--- launch_train_mlp.sh
+++ launch_train_mlp.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 #sbatch --job-name=KQH_mlp --gres=gpu:1 --mem=65536 --cpus-per-task=4 --output=./output/output_train_mlp.out launch_train_mlp.sh
 
