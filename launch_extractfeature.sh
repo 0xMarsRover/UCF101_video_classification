a779a7726b5f56853d4a003c968e49c24718d473
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-10-29 15:02:05 +0000

    update .sh

--- launch_extractfeature.sh
+++ launch_extractfeature.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#sbatch --job-name=KQH_extrac --gres=gpu:1 --mem=32768 --cpus-per-task=4 launch_train_lrcn.sh
-
-#SBATCH --job-name=KQH_extrac
-#SBATCH --gres=gpu:0
-#SBATCH --mem=32768
-#SBATCH --cpus-per-task=4
+#sbatch --job-name=KQH_extFeat --gres=gpu:1 --mem=32768 --cpus-per-task=4 launch_extractfeatures.sh
 
 python3 extract_features.py
